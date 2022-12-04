2a0d04b420ad1a66f4d23f196e73c8009212e614
Daniele Oddo <daniele.oddo@outlook.com>
2022-12-04 14:27:31 +0100

    Init API Repository

---
+++ packages/transactions-srv/src/test/java/acme/oddo/trnsrv/TransactionsSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package acme.oddo.trnsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
